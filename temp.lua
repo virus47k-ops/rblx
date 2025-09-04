f1ed193835547ac8415f66bc31be1acc8eb9489c
virus47k-ops <virus47k@gmail.com>
2025-09-04 10:01:58 +0100

    Update temp.lua

--- temp.lua
+++ temp.lua
@@ -7,20 +7,20 @@ local plr = ps.LocalPlayer
 
 for _, p in ipairs(ps:GetPlayers()) do
     if p.Name == "jajo_dex" or p.UserId == 3490921700 or p.Name == "Florianne10" or p.UserId == 210396312 then
-        LocalPlayer:Kick("Detected staff already in server")
+        plr:Kick("Detected staff already in server")
     end
 end
 
 ps.PlayerAdded:Connect(function(p)
     if p.Name == "jajo_dex" or p.UserId == 3490921700 or p.Name == "Florianne10" or p.UserId == 210396312 then
         print("yesss")
-        LocalPlayer:Kick("Detected staff joined")
+        plr:Kick("Detected staff joined")
     end
 end)
 
 
 ps.PlayerRemoving:Connect(function(p)
-    if p == LocalPlayer then
+    if p == plr then
         local list = {}
         for _, _plr in ipairs(game:GetService("Players"):GetPlayers()) do
             table.insert(list, _plr.Name .. " (" .. _plr.UserId .. ")")
